615fd7483c9c65c6d0f1647b7107a49ddc0bfe07
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2021-05-06 11:42:42 +0700

    🔀 feat: modify server qna service to use api (#2)

--- services/question-and-answer.service.js
+++ services/question-and-answer.service.js
@@ -1,28 +1,11 @@
-const qnaList = [
-  {
-    id: 1,
-    question: `Suppose you want to develop a supervised machine learning model to predict whether a given email is "spam" or "not spam." Which of the following statements are true?`,
-    awswer: `Emails not marked as "spam" or "not spam" are unlabeled examples.`,
-  },
-  {
-    id: 2,
-    question: `Suppose an online shoe store wants to create a supervised ML model that will provide personalized shoe recommendations to users. That is, the model will recommend certain pairs of shoes to Marty and different pairs of shoes to Janet. The system will use past user behavior data to generate training data. Which of the following statements are true?`,
-    awswer: `"The user clicked on the shoe's description" is a useful label.`,
-  },
-];
+const URI = 'http://localhost:3000';
 
 function getAll() {
-  return Promise.resolve(qnaList);
+  return fetch(URI).then(resp => resp.json()).then(data => data.qnaList)
 }
 
 function getAnswerByQuestion(id) {
-  return new Promise((resolve, reject) => {
-    const qna = qnaList.find((qna) => qna.id == id);
-    if (qna) {
-      return resolve(qna.awswer);
-    }
-    return reject(new Error("Could not find"));
-  });
+  return fetch(`${URI}/${id}`).then(resp => resp.json()).then(data => data.answer)
 }
 
 module.exports = {
